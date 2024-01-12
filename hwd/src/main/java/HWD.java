9505cec6754a8b0da6ec4f3bda5a543b31ef7eac
Anatolii <Steg92sm13@yandex.ru>
2024-01-13 00:11:11 +0800

    мой первый комит

--- hwd/src/main/java/HWD.java
+++ hwd/src/main/java/HWD.java
@@ -1,6 +1,9 @@
+import org.apache.commons.lang3.StringUtils;
+
 public class HWD {
 
     public static void main(String[] args) {
+        StringUtils.isAllLowerCase("hi");
         System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         System.out.println("HWD");
         System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
