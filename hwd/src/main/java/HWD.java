af82a00620b1e1862bd4e20e5d40f43dae8495dd
Yuri <yuri@legalmonkeys.com>
2024-01-12 22:41:10 +0700

    HWD

---
+++ hwd/src/main/java/HWD.java
@@ -0,0 +1,8 @@
+public class HWD {
+
+    public static void main(String[] args) {
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println("HWD");
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+}
